36e6bf086af5aa5234bf34c7394b219d463ce8bf
hp <eriktomala7@gmail.com>
2025-11-05 20:43:48 -0500

    coore

--- src/db.js
+++ src/db.js
@@ -1,9 +1,11 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const conmysql = await mysql.createConnection({
-  host: "interchange.proxy.rlwy.net",
-  user: "root",
-  password: "QlrjrTNaQsyWLMyRShLcFgKggactkQie",
-  database: "railway",
-  port: 27769
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
